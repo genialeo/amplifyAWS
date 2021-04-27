2fd2e320d4c6c148463d2b7116278b6350cf07c9
Your Name <lfenu@amazon.com>
2021-04-27 14:46:36 +0200

    prima release

--- amplifyAWS/amplifyAWSApp.swift
+++ amplifyAWS/amplifyAWSApp.swift
@@ -1,17 +1,39 @@
 //
-//  amplifyAWSApp.swift
-//  amplifyAWS
+//  amplifyApp.swift
+//  amplify
 //
-//  Created by Fenu, Leonardo on 27/04/2021.
+//  Created by Fenu, Leonardo on 23/04/2021.
 //
+
 
 import SwiftUI
 
+import Amplify
+import AWSDataStorePlugin
+
 @main
-struct amplifyAWSApp: App {
-    var body: some Scene {
-        WindowGroup {
-            ContentView()
-        }
+struct amplifyApp: App {
+  public init() {
+    configureAmplify()
+  }
+  var body: some Scene {
+    WindowGroup {
+      ContentView().environmentObject(AmicoStore())
     }
+  }
+}
+
+func configureAmplify() {
+  let dataStorePlugin = AWSDataStorePlugin(modelRegistration: AmplifyModels())
+  //let apiPlugin = AWSAPIPlugin(modelRegistration: AmplifyModels()) // UNCOMMENT this line once backend is deployed
+  
+  do {
+    try Amplify.add(plugin: dataStorePlugin)
+    //try Amplify.add(plugin: apiPlugin) // UNCOMMENT this line once backend is deployed
+    try Amplify.configure()
+    print("Initialized Amplify");
+  } catch {
+    // simplified error handling for the tutorial
+    print("Could not initialize Amplify: \(error)")
+  }
 }
