21f95c6ed3e4fc28e2449a1a325f996ca1a7dc8d
Your Name <lfenu@amazon.com>
2021-04-27 14:48:05 +0200

    new files added

---
+++ amplifyAWS/AmicoList.swift
@@ -0,0 +1,53 @@
+//
+//  AmicoList.swift
+//  amplify
+//
+//  Created by Fenu, Leonardo on 23/04/2021.
+//
+
+import SwiftUI
+
+struct AmicoList: View {
+  @EnvironmentObject var amicoStore: AmicoStore
+  @State private var showingAddAmico = false
+  
+  var body: some View {
+    NavigationView {
+      List {
+        ForEach(amicoStore.amici, id: \.id) { amico in
+          AmicoRowView(amico: amico)
+        }
+      }
+      .navigationBarTitle(Text("Amici"))
+      .navigationBarItems(
+        trailing:
+          Button(action: { showingAddAmico = true
+          }) {
+            Image(systemName: "plus")
+          }
+      )
+      .sheet(
+        isPresented: $showingAddAmico,
+        content: AddAmico.init
+      )
+    }
+  }
+}
+
+struct AmicoRowView: View {
+  var amico: Amico
+  var body: some View {
+    VStack {
+      if let nome = amico.nome {
+      Text(nome)
+        .font(.title)
+      }
+    }
+  }
+}
+
+struct AmicoList_Previews: PreviewProvider {
+  static var previews: some View {
+    AmicoList()
+  }
+}
