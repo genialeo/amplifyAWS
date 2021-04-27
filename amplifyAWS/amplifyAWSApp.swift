35162fa098ad0e68c6a1db2d381cc6dbb159812b
Your Name <lfenu@amazon.com>
2021-04-27 14:38:32 +0200

    Initial Commit

---
+++ amplifyAWS/amplifyAWSApp.swift
@@ -0,0 +1,17 @@
+//
+//  amplifyAWSApp.swift
+//  amplifyAWS
+//
+//  Created by Fenu, Leonardo on 27/04/2021.
+//
+
+import SwiftUI
+
+@main
+struct amplifyAWSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
